81307dd7bdbd3b462fa80b238113eaaf187c5761
Incinerator67 <edemosmanov2699@gmail.com>
2023-02-10 17:01:18 +0300

    Массивы в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,11 @@
 <script>
 export default {
-  data() {return {num1: 1,num2: 2,num3: 3,}
+  data() {return {arr: [1, 2, 3],}
 }
 }
 </script>
 <template>
-<div>{{ num1 + num2 + num3 }}</div> 
+<div>{{ sum(arr) }}</div> 
 </template>
 
 <style scoped>

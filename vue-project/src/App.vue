17c203d5e265bcc33433a77ef16771e2a581db06
Incinerator67 <edemosmanov2699@gmail.com>
2023-02-13 21:29:34 +0300

    омпонент для добавления данных в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,45 +1,40 @@
 <script>
-import User from './components/icons/User.vue'
-	export default {
-    components: {User},
-    users: [
-			{
-				id: 1,
-				name: 'name1',
-				surn: 'surn1'
-			},
-			{
-				id: 2,
-				name: 'name2',
-				surn: 'surn2'
-			},
-			{
-				id: 3,
-				name: 'name3',
-				surn: 'surn3'
-			},
-		]
-    methods: {
-      change(id, name, surn) {this.users = this.users.map((user) => {
-			if (user.id === id) {
-				user.name = name;
-				user.surn = surn;
-			}
-			return user;
-		});
-	}
-}
+  import User from './components/icons/User.vue'
+  export default {components: {User},
+      data() {return {
+        users: [
+          {
+            id: 1,
+            name: 'name1',
+            surn: 'surn1'
+          },
+          {
+            id: 2,
+            name: 'name2',
+            surn: 'surn2'
+          },
+          {
+            id: 3,
+            name: 'name3',
+            surn: 'surn3'
+          },
+        ],
+      }
+    },
+  methods: {
+    add(name, surn) {
+      let id = this.users.length + 1;
+      this.users.push({
+        id,
+        name,
+        surn
+      });
+    }
+  }
   }
 </script>
 <template>
- <User
-		v-for   ="user in users"
-		:id     ="user.id"
-		:name   ="user.name"
-		:surn   ="user.surn"
-		:key    ="user.id"
-		@change="change"
-	/>
+ <User Form @add="add" />
 </template>
 <style scoped>
 header {line-height: 1.5;}
--- vue-project/src/components/User.vue
+++ vue-project/src/components/User.vue
@@ -1,43 +1,18 @@
 <script>
 	export default {
-		props: {
-            id:     Number,
-            name:   String,
-            surn:   String,
+        data() {
+            return {
+                newName: '',
+                newSurn: '',
+            }
         },
-    emits: ['change'],
-    data() {
-        return {
-            isEdit: false,
-            newName: this.name,
-            newSurn: this.surn,
-        }
-    },
-    methods: {
-        edit() {
-            this.isEdit = true;
-        },
-        save() {
-            this.isEdit = false;
-            this.$emit('change', this.id, this.newName, this.newSurn);
-        }
-    }
-            
+    methods: {save() {this.$emit('add', this.newName, this.newSurn);}}
 }
 </script>
 <template>
-	<template v-if="!isEdit">
-		{{ name }}
-		{{ surn }}
-		<button @click="edit">
-			edit
-		</button>
-	</template>
-	<template v-else>
-		<input v-model="newName">
-		<input v-model="newSurn">
-		<button @click="save">
-			save
-		</button>
-	</template>
+	<input v-model="newName">
+	<input v-model="newSurn">
+	<button @click="save">
+		save
+	</button>
 </template>

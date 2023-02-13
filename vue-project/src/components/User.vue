cfc9daa6a4fb7c6f5dc2fa2bb8422d9541ce4d56
Incinerator67 <edemosmanov2699@gmail.com>
2023-02-13 20:40:50 +0300

    Создание компонентов в цикле в Vue

--- vue-project/src/components/User.vue
+++ vue-project/src/components/User.vue
@@ -1,13 +1,35 @@
 <script>
 	export default {
-		props: {
-		name: String,
-		surn: String
-    },
-	data() {return {name: 'name',surn: 'surname',}}
-    }
+	
+		data() {
+			return {
+				users: [
+			{
+				id: 1,
+				name: 'name1',
+				salary: 100,
+				age: 30,
+			},
+			{
+				id: 2,
+				name: 'name2',
+				salary: 200,
+				age: 40,
+			},
+			{
+				id: 3,
+				name: 'name3',
+				salary: 300,
+				age: 50,
+			},
+		],
+			}
+		}
+	}
 </script>
 <template>
+	{{ id  }}
 	{{ name }}
-	{{ surn }}
+	{{ salary }}
+	{{ age }}
 </template>

fb5e0bd81c749336e0454e3cf3bc08472a0fbbd8
Incinerator67 <edemosmanov2699@gmail.com>
2023-02-13 21:14:32 +0300

    еактивное удаление компонентов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,40 @@
 <script>
 import User from './components/icons/User.vue'
 	export default {
-    components: {User}
+    components: {User},
+    users: [
+			{
+				id: 1,
+				name: 'name1',
+				surn: 'surn1'
+			},
+			{
+				id: 2,
+				name: 'name2',
+				surn: 'surn2'
+			},
+			{
+				id: 3,
+				name: 'name3',
+				surn: 'surn3'
+			},
+		]
     methods: {
-      func() {alert('xxx');}
+      remove(id) {
+        this.users = this.users.filter((user) => {return user.id !== id;})
+      }
     }
   }
 </script>
 <template>
-  <User @show="func" />
-  <button @click="handle">btn</button>
+  <User
+		v-for   ="user in users"
+		:id     ="user.id"
+		:name   ="user.name"
+		:surn   ="user.surn"
+		@remove ="remove"
+		:key    ="user.id"
+	/>
 </template>
 <style scoped>
 header {line-height: 1.5;}
